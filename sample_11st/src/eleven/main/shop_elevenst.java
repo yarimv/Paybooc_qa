c8615b4554b15ad5ef0830b4486888b15c52c5a4
VP-QA5 <vpeggplanttest1@gmail.com>
2020-09-22 17:51:38 +0900

    200922
    1. TC013 작성 완료
    
    2. TC015 로그인 체크 로직 코딩
    - 로그인 안됐을 때 id 입력, pw 입력은 작성 완료

---
+++ sample_11st/src/eleven/main/shop_elevenst.java
@@ -0,0 +1,155 @@
+package eleven.main;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class shop_elevenst {
+	
+	//따로 돌리기 위해 선언
+	public static URL url;
+	public static DesiredCapabilities dc;
+	public static AndroidDriver<MobileElement> driver;
+	
+	public static ArrayList<userInfo> list = new ArrayList<userInfo>(); // userInfo 값을 리스트로 사용하기위해 선언
+	public static Scrolling scrolling = new Scrolling();
+	
+
+	@BeforeTest
+	public void setupAppium() throws MalformedURLException {
+		Userexcel info = new Userexcel();
+		list = info.excelUserinfo();
+
+		final String URL_STRING = "http://127.0.0.1:30300/wd/hub";
+		url = new URL(URL_STRING);
+
+		dc = new DesiredCapabilities();
+
+		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy Note8");
+
+		dc.setCapability("appPackage", "com.elevenst");
+		dc.setCapability("appActivity", "com.elevenst.intro.Intro");
+
+		// app 실행 시 데이터 초기화 방지 스크립트
+		dc.setCapability("noReset", true);
+		dc.setCapability("fullReset", false);
+
+		driver = new AndroidDriver<MobileElement>(url, dc);
+
+	}
+
+	/*
+	 * @AfterTest public void closeApp () throws InterruptedException {
+	 * driver.closeApp (); }
+	 */
+	public void findElement(String elementID) throws InterruptedException {
+
+		String[] array = elementID.split("/"); // elementID 주소를 / 기준으로 잘라서 출력
+
+		try {
+			driver.findElementById(elementID);
+			System.out.println(array[array.length - 1] + " 확인");
+
+		} catch (Exception e) {
+			System.out.println(array[array.length - 1] + " 못찾음");
+		}
+
+		Thread.sleep(2000);
+
+	}
+
+	public void xPathClick(String elementID) throws InterruptedException {
+
+		String[] array = elementID.split("/"); // elementID 주소를 / 기준으로 잘라서 출력
+
+		try {
+			driver.findElementById(elementID).click();
+			System.out.println(array[array.length - 1] + " 클릭");
+
+		} catch (Exception e) {
+			System.out.println(array[array.length - 1] + " 클릭 실패");
+		}
+
+		Thread.sleep(2000);
+	}
+	
+	@Test
+	public void TC013 () throws InterruptedException {
+		
+		//열려있는 모든 앱 닫기
+		((AndroidDriver<MobileElement>) driver).pressKey(new KeyEvent(AndroidKey.APP_SWITCH));
+		Thread.sleep(2000);
+
+		String clearapp8 = "com.android.systemui:id/recents_close_all_button"; //안드8
+		xPathClick(clearapp8);
+		
+		String clearapp9 = "com.sec.android.app.launcher:id/clear_all_button"; //안드9
+		xPathClick(clearapp9);
+		
+		//11번가 앱 실행 및 
+		driver.launchApp();
+		
+		Thread.sleep(5000);
+		
+		String elevenLogo = "com.elevenst:id/gnb_logo";
+		findElement(elevenLogo);
+		
+		Thread.sleep(3000);
+		
+	}
+	
+	//TC014 11번가 광고 화면, 팝업이 없어서 일단 건너 뜀
+	
+	@Test //11번가 로그인 확인
+	public void TC015 () throws InterruptedException {
+		
+		//11번가 마이 화면으로 이동
+		String myEleven = "com.elevenst:id/btn_my";
+		xPathClick(myEleven);
+		
+		Thread.sleep(3000);
+		
+		//if 마이페이지 [회원정보] 버튼 찾기 else if 로그인 프로세스 시작
+			if (driver.findElement(By.xpath("//android.view.View[@text='나의 11번가']")) != null) {
+				System.out.println("로그인 확인");
+				
+			} else {
+				
+				//appium send text로 쇼핑몰 id 입력
+				MobileElement inputID = (MobileElement) driver.findElement(By.xpath("//android.widget.EditText[@resource-id='userId']"));
+				inputID.click();
+				inputID.sendKeys("inmanaged"); //userinfo 엑셀 값 넣어야 함
+				
+				Thread.sleep(1000);
+				
+				//appium send text로 쇼핑몰 pw 입력
+				MobileElement inputPW = (MobileElement) driver.findElement(By.xpath("//android.widget.EditText[@resource-id='userPw']"));
+				inputPW.click();
+				inputPW.sendKeys("qmdlvl1!"); //userinfo 엑셀 값 넣어야 함
+				
+				Thread.sleep(1000);
+				
+				//로그인 버튼 클릭
+				driver.findElement(By.xpath("//android.widget.Button[@text='로그인']")).click();
+			}
+			
+		}
+
+	
+	
+
+}
